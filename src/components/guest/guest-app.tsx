fef3425360018d7216790c60a31efc238e507150
lim <lim@limui-MacBookAir.local>
2024-10-09 23:05:21 +0900

    feat: robots.txt 추가 및 bg 설정

--- src/components/guest/guest-app.tsx
+++ src/components/guest/guest-app.tsx
@@ -5,25 +5,25 @@ const SignIn = lazy(() => import("./sign-in"));
 const SignUp = lazy(() => import("./sign-up"));
 
 interface GuestAppProps {
-	login: (user: ReadUserDto) => void;
+  login: (user: ReadUserDto) => void;
 }
 
 function GuestApp({ login }: GuestAppProps) {
-	const [mode, setMode] = useState<"in" | "up">("in");
+  const [mode, setMode] = useState<"in" | "up">("in");
 
-	const changeMode = (mode: "in" | "up" = "in") => {
-		setMode(mode);
-	};
+  const changeMode = (mode: "in" | "up" = "in") => {
+    setMode(mode);
+  };
 
-	return (
-		<div className="flex justify-center items-center h-[100vh] bg-[url('/login.webp')] bg-center">
-			{mode === "in" ? (
-				<SignIn login={login} changeMode={changeMode} />
-			) : (
-				<SignUp changeMode={changeMode} />
-			)}
-		</div>
-	);
+  return (
+    <div className="flex justify-center items-center h-[100vh] bg-gray-200 bg-center">
+      {mode === "in" ? (
+        <SignIn login={login} changeMode={changeMode} />
+      ) : (
+        <SignUp changeMode={changeMode} />
+      )}
+    </div>
+  );
 }
 
 export default GuestApp;
